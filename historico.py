b37d070ae99be69079a2d7538e2e1903cdb8d9d3
Gwaean (Nicole) <56048874+Gwaean@users.noreply.github.com>
2023-11-18 14:52:25 -0300

    Update historico.py

--- historico.py
+++ historico.py
@@ -1,1 +1,5 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QHBoxLayout, QSizePolicy
+from PySide6.QtGui import QPainter, QColor
+from PySide6.QtCore import Qt
 
+class NovaTela(QMainWindow):

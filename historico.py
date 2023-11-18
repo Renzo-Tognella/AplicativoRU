a2d895d38cdd85c3458815a7b530f4439583aad0
Gwaean (Nicole) <56048874+Gwaean@users.noreply.github.com>
2023-11-18 15:42:20 -0300

    Update historico.py

--- historico.py
+++ historico.py
@@ -3,4 +3,44 @@ from PySide6.QtGui import QPainter, QColor
 from PySide6.QtCore import Qt
 
 class HistoricocomprasApp:
-def __init__(self):
+def __init__(self, cor_fundo=(248, 237, 237):
+  super(HistoricoComprasApp, self).__init__()
+
+        self.setWindowTitle("Histórico de Compras de Tickets")
+
+        # Criar um layout vertical
+        layout = QVBoxLayout(self)
+         # Definir o tipo de fonte
+        font = QFont("Biome Light", 16)  
+        label.setFont(font)
+
+        # Criar a primeira caixa de cor cinza
+        caixa1 = QFrame(self)
+        caixa1.setFrameShape(QFrame.StyledPanel)
+        caixa1.setStyleSheet("background-color: rgb(200, 200, 200);")  # Cor cinza
+
+        # Adicionar informações à primeira caixa
+        label1 = QLabel("Tipo de Refeição: Almoço\nData e Hora: 01/01/2023 12:30\nLocal: UTFPR Campus Curitiba - Centro", caixa1)
+        font1 = QFont("Biome Light", 12)  
+        label1.setFont(font1)
+        layout.addWidget(caixa1)
+
+        # Criar a segunda caixa de cor cinza
+        caixa2 = QFrame(self)
+        caixa2.setFrameShape(QFrame.StyledPanel)
+        caixa2.setStyleSheet("background-color: rgb(200, 200, 200);")  # Cor cinza
+      
+        # Adicionar informações à segunda caixa
+        label2 = QLabel("Tipo de Refeição: Jantar\nData e Hora: 05/02/2023 19:00\nLocal: UTFPR Campus Curitiba - Ecoville", caixa2)
+        font2 = QFont("Biome Light", 12)  
+        label2.setFont(font2)
+        layout.addWidget(caixa2)+ONM 
+
+        # Configurar o layout do widget principal
+        self.setLayout(layout)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = HistoricoComprasApp()
+    window.show()
+    app.exec()

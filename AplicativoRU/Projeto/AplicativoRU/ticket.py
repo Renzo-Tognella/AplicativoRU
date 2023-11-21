a45d5f8a23a11a370b3e24e0a95323fde3788fc0
Renzo Tognella <122568128+Renzo-Tognella@users.noreply.github.com>
2023-11-21 17:59:16 -0300

    Add files via upload

---
+++ AplicativoRU/Projeto/AplicativoRU/ticket.py
@@ -0,0 +1,8 @@
+
+class Ticket:
+     def __init__(self, ticket_id, usado, dia, cpf_pessoa, horario):
+        self.ticket_id = ticket_id
+        self.usado = usado
+        self.dia = dia
+        self.cpf_pessoa = cpf_pessoa
+        self.horario = horario

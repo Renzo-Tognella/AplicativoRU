0d85eef9b7ce9689fe33684848ee8e7221eec6b9
Renzo Tognella <122568128+Renzo-Tognella@users.noreply.github.com>
2023-11-21 18:01:19 -0300

    Delete AplicativoRU directory

--- AplicativoRU/Projeto/AplicativoRU/funcionario.py
+++
@@ -1,7 +0,0 @@
-from pessoa import Pessoa
-
-class Funcionario(Pessoa):
-    def __init__(self, cpf, nome, saldo, registro, cargo):
-        super().__init__(cpf, nome, saldo)
-        self.cargo = registro
-        self.cargo = cargo
